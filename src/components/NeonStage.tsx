654ae6a822848c160f9d20b46b481b8a73cd5878
thy <213379314+okaythi@users.noreply.github.com>
2026-09-23 02:58:50 +0200

    perf: optimize point light cubemap shadow size to 1024

--- src/components/NeonStage.tsx
+++ src/components/NeonStage.tsx
@@ -466,8 +466,8 @@ function StageModel(props: NeonStageProps) {
         distance={35}
         decay={2}
         castShadow
-        shadow-mapSize-width={2048}
-        shadow-mapSize-height={2048}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
         shadow-radius={3.5}
         shadow-bias={-0.0001}
       />
